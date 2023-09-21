e397a7360d14f348edf594f1bddefa24d9b43b84
63070078 <73688566+63070078@users.noreply.github.com>
2023-09-21 16:42:05 +0700

    Update config.js ( password )

--- itjs_backend/config.js
+++ itjs_backend/config.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2/promise");
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: '789067ploy',
+  password: 'password',
   database: 'itjs',
   connectionLimit: 10,
 });
