375bc61e77e42d5bd973f909b5d97b3e18d221f2
tnploy <63070078@kmitl.ac.th>
2023-12-06 17:05:11 +0700

    เพิ่ม env

--- itjs_backend/index.js
+++ itjs_backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require("cors");
+require("dotenv").config(); //ใข้งาน .env
 const path = require('path');
 const router = express.Router();
 app.use(cors());
